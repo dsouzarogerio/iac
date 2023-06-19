aca8b00b2be658ef6d84c3940dc7801b5a98aac1
Rogério de Souza <dsouza.rogerio@gmail.com>
2023-06-19 11:51:05 -0300

    commit inicial

---
+++ main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami  = "ami-03f65b8614a860c29"
+  instance_type = "t2.micro"
+  key_name = "teste-alura"
+  # user_data = <<-EOF
+  #               #!/bin/bash
+  #               cd /home/ubuntu
+  #               echo "<h1>Feito com Terraform</h1>" > index.html
+  #               nohup busybox httpd -f -p 8080 &
+  #               EOF
+  tags = {
+    Name = "Teste AWS"
+  }
+}
