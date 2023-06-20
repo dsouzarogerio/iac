3583e0946bde6ddf5f9d4e3a77e32133283466bc
Rogério de Souza <dsouza.rogerio@gmail.com>
2023-06-20 13:28:36 -0300

    criando ambiente virtual com python

--- main.tf
+++ main.tf
@@ -24,6 +24,6 @@ resource "aws_instance" "app_server" {
   #               nohup busybox httpd -f -p 8080 &
   #               EOF
   tags = {
-    Name = "Teste AWS_1"
+    Name = "Terraform Ansible Python"
   }
 }
